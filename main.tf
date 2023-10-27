a3a2b19a2604b6f082e4c52f8e9339195ab7fa53
AT Will <at_soamazed@icloud.com>
2023-10-26 21:01:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,3 +50,26 @@ resource "aws_ecs_service" "app_service" {
     }
   }
 
+
+resource "aws_security_group" "traefik_sg" {
+  name        = "${var.app_name}-traefik-sg"
+  description = "Allow access to Traefik for ${var.app_name}"
+  vpc_id      = var.vpc_id
+
+  # Allows traffic from the SG to traefik
+  ingress {
+    from_port       = var.container_port
+    to_port         = var.container_port
+    protocol        = "tcp"
+    security_groups = ["${var.security_group_id}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.app_name}-traefik-sg"
+  }
+}
